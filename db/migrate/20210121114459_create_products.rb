8ce00b127c841e6d723b43a3831afb45b2ba84cf
Hamilton Kamiya <31168921+enghamilton@users.noreply.github.com>
2021-01-22 02:50:23 -0500

    Create 20210121114459_create_products.rb

---
+++ db/migrate/20210121114459_create_products.rb
@@ -0,0 +1,12 @@
+class CreateProducts < ActiveRecord::Migration[6.1]
+  def change
+    create_table :products do |t|
+      t.int :id
+      t.text :username
+      t.text :phone
+      t.text :price
+
+      #t.timestamps
+    end
+  end
+end
