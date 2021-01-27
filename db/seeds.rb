396e83fe0dcd1599c1e629560470d673e402218a
Hamilton Kamiya <31168921+enghamilton@users.noreply.github.com>
2021-01-27 10:26:14 -0500

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Product.create(username: "hamilton", phone: "912345678", price: "100.00")
-Product.create(username: "user 01", phone: "900000001", price: "100.00")
-Product.create(username: "user 02", phone: "900000002", price: "100.00")
+Product.create(pid: 1, username: "hamilton", phone: "912345678", price: "100.00")
+Product.create(pid: 2, username: "user 01", phone: "900000001", price: "100.00")
+Product.create(pid: 3, username: "user 02", phone: "900000002", price: "100.00")
