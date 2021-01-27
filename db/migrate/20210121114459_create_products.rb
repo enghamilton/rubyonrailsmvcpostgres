3a4e2d51a17f9050d93b39021be344b0bb6a042a
Hamilton Kamiya <31168921+enghamilton@users.noreply.github.com>
2021-01-27 10:31:10 -0500

    Update 20210121114459_create_products.rb

--- db/migrate/20210121114459_create_products.rb
+++ db/migrate/20210121114459_create_products.rb
@@ -1,7 +1,7 @@
 class CreateProducts < ActiveRecord::Migration[6.1]
   def change
     create_table :products do |t|
-      t.int :pid
+      t.int :id
       t.text :username
       t.text :phone
       t.text :price
